5557b42bbf0db7b225fcfdbb4867f85a817b977d
Makinde Mayowa <mayowamakinde23@gmail.com>
2024-10-08 09:10:07 +0100

    Update Hero.tsx

--- components/landing/Hero.tsx
+++ components/landing/Hero.tsx
@@ -68,7 +68,7 @@ export default function Hero({ setRole, signupRole }: any) {
 
             {/* Right Section: Illustration Slider */}
             {/* <div className="hidden md:flex w-full md:w-1/2 items-center justify-center"> */}
-            <div className="flex w-full md:w-1/2 items-center justify-center">
+            <div className="flex w-full w-1/2 items-center justify-center">
               <Slider {...settings} className="w-2/3 mx-auto">
                 {illustrations.map((illustration, index) => (
                   <div key={index} className="flex justify-center">
